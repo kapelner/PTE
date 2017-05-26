75b8b6bf41b5d0e79e8be2ce24d0e7b61cbbb213
Adam Kapelner <kapelner@gmail.com>
2017-05-26 13:05:48 -0400

    ROxygen compiles properly

---
+++ PTE/R/PTE.R
@@ -0,0 +1,12 @@
+#' Personalized Medicine...
+#'
+#' @name 		PTE
+#' @docType 	package
+#' @title 		Personalized Medicine Inference
+#' @author 		Adam Kapelner \email{kapelner@@qc.cuny.edu}, Alina Levine and Justin Bleich
+#' @references 	Kapelner, A, Bleich, J, Cohen, ZD, DeRubeis, RJ and Berk, R (2014) Inference for Treatment Regime Models in Personalized Medicine, arXiv
+#' @keywords 	Personalized medicine, bootstrap
+#' @import      survival stats graphics
+##### Run "library(roxygen2); roxygenise("PTE", clean = TRUE)" to regenerate all Rd files and NAMESPACE and DESCRIPTION file
+##### but make sure you are in the root directory of the project
+NULL
